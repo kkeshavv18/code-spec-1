95d07d223a83984f73809142fb630e5fd8bcd9c7
Keshav Kattel <114460628+kkeshavv18@users.noreply.github.com>
2023-05-26 10:22:47 -0700

    Add files via upload

---
+++ codespec-logo/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="logo-contaier">
+        <span className="logo-text1 logo-text">CODE</span>
+        <span className="logo-text2 logo-text">SPEC</span>
+      </div>
+    </div>
+  );
+}
